800d17e955dc97b584d12b95aab024b6de3e381b
dcsan <dc@rikai.co>
2018-06-18 00:35:51 +0800

    made a Piny namespace to allow access to internal methods

--- pinyinify.js
+++ pinyinify.js
@@ -4,60 +4,94 @@ let pinyin = require("pinyin"),
 
 let punctuation = new Set("！？，。：；’”）%~@#^&*");
 
-function pinyinify(text) {
-    text = spacePunctuation(text);
-    let cut = nodejieba.cut(text);
-    let out = "", prevIsCharacter = false;
-    cut.forEach((text) => {
-        let word;
-        if (pinyinDict[text]) {
-            word = pinyinDict[text];
-        } else {
-            let arr = pinyin(text, {
-                heteronym: true,
-                segment: true
-            });
-            word = arr.map((x) => x[0]).join("");
-        }
-        if (prevIsCharacter && ! punctuation.has(text)) {
-            out += " " + word;
-        } else {
-            out += word;
-        }
-        prevIsCharacter = word !== text;
-    });
-    return fixPunctuation(out);
-}
+const Piny = {
 
-function spacePunctuation(text) {
-    return text.replace(/([！？，。：；’”%）]+)([^ ！？，。：；’”%）])/g, (x,p,n) => p + " " + n)
-        .replace(/([0-9]+)([^ 0-9\.\?\!\)\]\}！？，。：；’”）%~\@\#\^\&\*])/g, (x,p,n) => p + " " + n);
-}
-
-function fixPunctuation(text) {
-    let replacements = {
-        "！": "!",
-        "？": "?",
-        "。": ".",
-        "，": ",",
-        "：": ":",
-        "；": ";",
-        "‘": "`",
-        "’": "'",
-        "“": "``",
-        "”": "\"",
-        "（": "(",
-        "）": ")"
-    };
-    let newString = "";
-    for (let char of text) {
-        if (char in replacements) {
-            newString += replacements[char];
-        } else {
-            newString += char;
+    // return all the working parts too
+    convert(text) {
+        text = Piny.spacePunctuation(text);
+        let cut = nodejieba.cut(text, true);
+        let out = "", prevIsCharacter = false;
+        cut.forEach((text) => {
+            let word;
+            if (pinyinDict[text]) {
+                word = pinyinDict[text];
+            } else {
+                let arr = pinyin(text, {
+                    heteronym: true,
+                    segment: true
+                });
+                word = arr.map((x) => x[0]).join("");
+            }
+            if (prevIsCharacter && ! punctuation.has(text)) {
+                out += " " + word;
+            } else {
+                out += word;
+            }
+            prevIsCharacter = word !== text;
+        });
+        let result = {
+            pn: Piny.fixPunctuation(out),
+            cut: cut
         }
+        return result;
+    },
+
+    pinyinify(text) {
+        text = Piny.spacePunctuation(text);
+        let cut = nodejieba.cut(text);
+        let out = "", prevIsCharacter = false;
+        cut.forEach((text) => {
+            let word;
+            if (pinyinDict[text]) {
+                word = pinyinDict[text];
+            } else {
+                let arr = pinyin(text, {
+                    heteronym: true,
+                    segment: true
+                });
+                word = arr.map((x) => x[0]).join("");
+            }
+            if (prevIsCharacter && ! punctuation.has(text)) {
+                out += " " + word;
+            } else {
+                out += word;
+            }
+            prevIsCharacter = word !== text;
+        });
+        return Piny.fixPunctuation(out);
+    },
+
+    spacePunctuation(text) {
+        return text.replace(/([！？，。：；’”%）]+)([^ ！？，。：；’”%）])/g, (x,p,n) => p + " " + n)
+            .replace(/([0-9]+)([^ 0-9\.\?\!\)\]\}！？，。：；’”）%~\@\#\^\&\*])/g, (x,p,n) => p + " " + n);
+    },
+
+    fixPunctuation(text) {
+        let replacements = {
+            "！": "!",
+            "？": "?",
+            "。": ".",
+            "，": ",",
+            "：": ":",
+            "；": ";",
+            "‘": "`",
+            "’": "'",
+            "“": "``",
+            "”": "\"",
+            "（": "(",
+            "）": ")"
+        };
+        let newString = "";
+        for (let char of text) {
+            if (char in replacements) {
+                newString += replacements[char];
+            } else {
+                newString += char;
+            }
+        }
+        return newString;
     }
-    return newString;
+
 }
 
-module.exports = pinyinify;
+module.exports = Piny;
